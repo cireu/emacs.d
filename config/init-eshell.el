da5fd0286caed12afdc2b40b505d2cfee7dc31c0
cireu <all_but_last@163.com>
2018-11-18 18:33:17 +0800

    Update

--- config/init-eshell.el
+++
@@ -1,8 +0,0 @@
-(use-package aweshel
-  :load-path "site-lisp/lazycat-collection/aweshell"
-  :commands (aweshell-toggle
-             aweshell-next
-             aweshell-prev)
-  :general
-  (l-spc
-    "ae" 'aweshell-toggle))
